9cbad87c8239248bb15b16f07012420a3b71acae
Swami09 <swami.91196@gmail.com>
2022-09-09 13:22:14 +0530

    updated till API 91

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 
-const App= ()=> {
-    return <SearchBar />
+class App extends React.Component {
+    handleSubmit(term){
+        console.log(term);
+    }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.handleSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
-class SearchBar extends React.Component{
-    render(){
-        return(
-            <div>
-                <form>
-                    <input type="text" />
-                </form>
-            </div>
-        )
-    }
+class SearchBar extends React.Component {
+state= {term: ""};
+
+handleSubmit= (event)=> {
+    event.preventDefault();   
+    this.props.onSubmit(this.state.term)
+}
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" value={this.state.term} onChange={(e)=>this.setState({term: e.target.value})}/>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
